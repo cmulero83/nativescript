636121d8cbb4bb750d0005dd6416e980fb964603
Carlos Mulero <cmulero83@icloud.com>
2020-08-01 19:14:51 -0500

    Semana3 final

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,27 @@ import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 import { NativeScriptModule } from "nativescript-angular/nativescript.module";
 import { NativeScriptUISideDrawerModule } from "nativescript-ui-sidedrawer/angular";
 
+import { EffectsModule } from "@ngrx/effects";
+import { ActionReducerMap, StoreModule as NgRxStoreModule } from "@ngrx/store";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
+import { initializeNoticiasState, NoticiasEffects, NoticiasState, reducersNoticias } from "./domain/noticias.state.model";
 
 import { NoticiasService } from "./domain/noticias.serve";
+
+// Redux init
+
+export interface AppState {
+
+        noticias: NoticiasState;
+    }
+    
+    const reducersInitialState = {
+    
+        noticias: initializeNoticiasState()
+    };
+    
+    // Fin redux init
 
 @NgModule({
     bootstrap: [
@@ -14,7 +31,9 @@ import { NoticiasService } from "./domain/noticias.serve";
     imports: [
         AppRoutingModule,
         NativeScriptModule,
-        NativeScriptUISideDrawerModule
+        NativeScriptUISideDrawerModule,
+        NgRxStoreModule.forRoot(reducers, { initialState: reducersInitialState}),
+        EffectsModule.forRoot([NoticiasEffects])
     ],
     providers:[NoticiasService],
     declarations: [
--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,8 +1,13 @@
-import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
+import { Component, OnInit, ElementRef, ViewChild } from "@angular/core";
+import { Store } from "@ngrx/store";
+import * as Toast from "nativescript-toasts";
 import { RadSideDrawer } from "nativescript-ui-sidedrawer";
 import * as app from "tns-core-modules/application";
+import { AppState } from "../app.module";
+import { Noticia, NuevaNoticiaAction } from "../domain/noticias.state.model";
 import { NoticiasService } from "../domain/noticias.serve";
-import { View, Color } from "tns-core-modules/ui/page";
+
+
 
 @Component({
     selector: "Search",
@@ -12,17 +17,25 @@ export class SearchComponent implements OnInit {
     resultados: Array<string>;
     layout: ElementRef;
 
-    constructor(private noticias: NoticiasService) {
+    constructor(      private noticias: NoticiasService,
+        private store: Store<AppState>) 
+
+    {
         // Use the component constructor to inject providers.
+
+        
     }
 
     ngOnInit(): void {
         // Init your component properties here.
+        this.store.select((state) => state.noticias.sugerida)
+            .subscribe((data) => {
+                const f = data;
 
-        this.noticias.agregar("Hola");
-        this.noticias.agregar("Holaa");
-        this.noticias.agregar("Hola5");
-        this.noticias.agregar("Hola33");
+                if (f != null) {
+                    Toast.show({text: "Sugerimos leer: " + f.titulo, duration: Toast.DURATION.SHORT});
+                }
+            });
 
     }
 
@@ -31,12 +44,26 @@ export class SearchComponent implements OnInit {
         sideDrawer.showDrawer();
     }
 
-    onItemTap(x): void {
-        console.dir(x);
-    }
+    onItemTap(args): void {
+        this.store.dispatch(new NuevaNoticiaAction(new Noticia(args.view.bindingContext)));
+    }git
 
     BuscarAhora(s: string){
-        this.resultados = this.noticias.buscar().filter((x) => x.indexOf(s) >= 0);
+
+        console.dir("buscarAhora" + s);
+        this.noticias.buscar(s).then((r: any) => {
+            console.log("resultados buscarAhora: " +JSON.stringify(r));
+            this.resultados = r;
+        }, (e) => {
+            console.log("error buscarAhora " + e);
+            Toast.show({text: "Error en la busqueda", duration: Toast.DURATION.SHORT})
+        })
+
+
+
+
+
+        /*this.resultados = this.noticias.buscar().filter((x) => x.indexOf(s) >= 0);
 
         const layout = <View>this.layout.nativeElement;
         
@@ -50,7 +77,7 @@ export class SearchComponent implements OnInit {
             backgroundColor: new Color("black"),
             duration: 3000,
             delay: 1500
-        }));
+        }));*/
 
 
     }
